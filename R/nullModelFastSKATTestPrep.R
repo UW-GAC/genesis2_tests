102ab53bbeb655ea464b46e69dddb97305de3924
Tamar Sofer <tsofer@hsph.harvard.edu>
2018-03-28 19:51:02 -0400

    Updated the function to work in linear regression
    Check if cholSigmaInv is a scalar, adapt various computations if it is, also adapt the fastSKAT object type.

--- R/nullModelFastSKATTestPrep.R
+++ R/nullModelFastSKATTestPrep.R
@@ -4,17 +4,22 @@ nullModelFastSKATTestPrep <- function(nullmod, threshold = 1e-10){
     Y <- nullmod$workingY
     W <- nullmod$model.matrix
     C <- nullmod$cholSigmaInv
-    
-    #sparseC <- Matrix(C) # should be already a Matrix
-    SIGMA <- chol2inv(t(C))
-    SIGMA[abs(SIGMA) < threshold] <- 0
-    SIGMA <- Matrix(SIGMA)
-    CholSigma <- t(chol(SIGMA)) 
-    SIGMAinv <- tcrossprod(C)
-    
- 
-    qr <- qr(as.matrix(solve(CholSigma,W)))
 
+	## C can also be a scalar (simple linear regression) - adapt code  
+	if (is.null(dim(C)) & length(C) == 1){
+		CholSigma <- 1/C
+		SIGMAinv <- C^2
+		qr <- qr(W/CholSigma)
+	} else{
+		
+  	  SIGMA <- chol2inv(t(C))
+  	  SIGMA[abs(SIGMA) < threshold] <- 0
+   	 SIGMA <- Matrix(SIGMA)
+   	 CholSigma <- t(chol(SIGMA)) 
+   	 SIGMAinv <- tcrossprod(C)
+
+   	 qr <- qr(as.matrix(solve(CholSigma,W)))
+	}
 
     out <- list(CholSigma = CholSigma, SIGMAinv = SIGMAinv, qr = qr, resids = nullmod$resid.marginal)
     return(out)
@@ -22,25 +27,54 @@ nullModelFastSKATTestPrep <- function(nullmod, threshold = 1e-10){
 
 
 
-.testVariantSetFastSKAT <- function(nullprep, G, weights, method=c("ssvd","lanczos","satterthwaite"), neig=100, tr2.sample.size=500, q=NULL,  convolution.method=c("saddlepoint","integration"), remainder.underflow=c("warn","missing","error")){
+.testVariantSetFastSKAT <- function(nullprep, G, weights,  rho = 0, method=c("ssvd","lanczos","satterthwaite"), neig=100, tr2.sample.size=500, q=NULL,  convolution.method=c("saddlepoint","integration"), remainder.underflow=c("warn","missing","error")){
     
-    sparseG <- Matrix(G, sparse = TRUE) %*% Diagonal(x = weights)
+    if (rho == 0){
+		sparseG <- Matrix(G, sparse = TRUE) %*% Diagonal(x = weights) 
+	} else{
+		rho.mat <- matrix(rho, nrow = length(weights), ncol = length(weights))
+		diag(rho.mat) <- 1
+		cholRhoMat <- t(chol(rho.mat, pivot=TRUE))
+		sparseG <- Matrix(G, sparse = TRUE) %*% Diagonal(x = weights)  %*% cholRhoMat
+	}
     
-    rval <- list(mult = function(X){
-    	base::qr.resid(nullprep$qr, as.matrix(solve(nullprep$CholSigma, (sparseG %*% X))))	
-    }, tmult = function(X){
-    	crossprod(sparseG, solve(t(nullprep$CholSigma), base::qr.resid(nullprep$qr,X)))
-    }, 
-    trace = NULL,
-    ncol = ncol(G),
-    nrow = nrow(G),
-    Q = function(){
-    	stdres <- nullprep$SIGMAinv %*% nullprep$resids
-    	s = crossprod(sparseG, stdres)
-    	sum(s^2)
-    })
+	if (is.null(dim(C)) & length(C) == 1){
+		  rval <- list(mult = function(X){
+    		base::qr.resid(nullprep$qr, as.matrix((sparseG %*% X)/nullprep$CholSigma))	
+	    }, tmult = function(X){
+	    	crossprod(sparseG, base::qr.resid(nullprep$qr,X)/nullprep$CholSigma)
+	    }, 
+	    trace = NULL,
+	    ncol = ncol(G),
+	    nrow = nrow(G),
+	    Q = function(){
+	    	stdres <- nullprep$SIGMAinv * nullprep$resids
+	    	s = crossprod(sparseG, stdres)
+	    	sum(s^2)
+	    })
+	    
+	    class(rval) <- c( "matrixfree") ## or famSKAT_genesis?
+	
+		} else{
+			   rval <- list(mult = function(X){
+		    	base::qr.resid(nullprep$qr, as.matrix(solve(nullprep$CholSigma, (sparseG %*% X))))	
+		    }, tmult = function(X){
+		    	crossprod(sparseG, solve(t(nullprep$CholSigma), base::qr.resid(nullprep$qr,X)))
+		    }, 
+		    trace = NULL,
+		    ncol = ncol(G),
+		    nrow = nrow(G),
+		    Q = function(){
+		    	stdres <- nullprep$SIGMAinv %*% nullprep$resids
+		    	s = crossprod(sparseG, stdres)
+		    	sum(s^2)
+		    })
+
+			class(rval) <- c("famSKAT_genesis", "famSKAT", "matrixfree") 
+			
+			}
     
-    class(rval) <- c("famSKAT_genesis", "famSKAT", "matrixfree") ## or famSKAT_genesis?
+
     
     pval <- bigQF::pQF(rval$Q(), rval,  method = method, neig = neig, tr2.sample.size = tr2.sample.size, q = q, convolution.method = convolution.method, remainder.underflow = remainder.underflow) ### what object should be the second entry of pQF??
    	names(pval) <- "pval"
